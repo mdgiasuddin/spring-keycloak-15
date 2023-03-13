0fab2f8509d7808d5d486bd1aa794908c2ef51c9
Md. Giash Uddin <giash.uddin@bfi.co.id>
2023-03-13 22:07:09 +0600

    Create user from application

--- src/main/java/com/example/springkeycloak/dtos/UserDTO.java
+++ src/main/java/com/example/springkeycloak/dtos/UserDTO.java
@@ -6,16 +6,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 public class UserDTO {
 
+    @NotBlank
     private String firstName;
     private String lastName;
+    @NotBlank
     private String username;
     private String email;
+    @NotBlank
     private String password;
+    @NotNull
     private UserRole role;
 }
